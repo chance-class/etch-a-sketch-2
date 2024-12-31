6c82c9ec7c1c5964636b729ec6e495b469424023
chance-class <129628753+chance-class@users.noreply.github.com>
2024-12-30 16:31:31 -0800

    Write and employ getRandomColor function
    Create function that chooses a series of random numbers and/or
    letters to form a hexadecimal color code, then set that color as
    the background color of the square currently being passed over.

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ button.addEventListener("click", () => {
 
 body.insertBefore(button, container);
 
+function getRandomColor() {
+  let chars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c', 'd', 'e', 'f'];
+  let arr = [];
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * (chars.length - 1));
+    arr.push(chars[index]);
+  }
+  return arr.join('');
+}
+
 function getNumberOfSquares() {
   let num = +prompt("Please enter a number between 16 and 100.", 16);
   if (num >= 16 && num <= 100) return num;
@@ -33,8 +43,11 @@ function createSquare(num) {
   square.classList.add("square");
   square.style.width = `${width}px`;
   square.style.height = `${height}px`;
+
   square.addEventListener("mouseover", () => {
-    square.classList.add("draw");
+    // square.classList.add("draw");
+    let bgColor = getRandomColor();
+    square.style.backgroundColor = `#${bgColor}`;
     opac += 0.1;
     square.style.opacity = opac;
   })
